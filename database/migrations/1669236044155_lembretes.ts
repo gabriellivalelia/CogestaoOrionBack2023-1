b54b3e4a3292c089e4388c00d3e3f83621b147f9
arthurpontespiazzaroli@gmail.com <arthurpontespiazzaroli@gmail.com>
2022-11-23 17:58:43 -0300

    Comite de migrations q eu fiz e vou atualizar

---
+++ database/migrations/1669236044155_lembretes.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'lembretes'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.string('id_lembrete').primary()
+      table.string('titulo').notNullable()
+      table.string('descricao').notNullable()
+      table.string('id_usuario').references('id_usuario').inTable('users')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
