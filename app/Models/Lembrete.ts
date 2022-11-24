89ee52ab6d488c3a0afa9c7a92d763d5a8526a7c
Natalia Mota <nataliamotand@gmail.com>
2022-11-24 00:14:50 -0300

    Models

---
+++ app/Models/Lembrete.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import {v4 as uuid} from 'uuid'
+import Usuario from './Usuario'
+
+export default class Lembrete extends BaseModel {
+
+  @column({ isPrimary: true })
+  public id_lembrete: string
+
+  @beforeCreate()
+  public static async createUUID(model:Lembrete){
+    model.id_lembrete = uuid()
+  }
+
+  @column()
+  public titulo: string
+
+  @column()
+  public descricao: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => Usuario, {
+    foreignKey: 'id_usuario'
+  })
+  public usuario: BelongsTo<typeof Usuario>
+}
---
+++ app/Models/Usuario.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import {v4 as uuid} from 'uuid'
+import Lembrete from './Lembrete'
+
+export default class Usuario extends BaseModel {
+
+  @column({ isPrimary: true })
+  public id_usuario: string
+
+  @beforeCreate()
+  public static async createUUID(model:Usuario){
+    model.id_usuario = uuid()
+  }
+
+  @column()
+  public nome: string
+
+  @column()
+  public email: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @hasMany(() => Lembrete, {
+    foreignKey: 'id_usuario'
+  })
+  public lembrete: HasMany<typeof Lembrete>
+}
