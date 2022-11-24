29692f71aff2003c04c142b2ba10d8b5003a31e6
nmota <nataliamotand@gmail.com>
2022-11-24 10:31:02 -0300

    Rotas

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,16 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+//Usuarios
+Route.get('/usuarios', 'UsuariosController.index')
+Route.post('/usuarios', 'UsuariosController.store')
+Route.delete('/usuarios/:id_usuario', 'UsuariosController.destroy')
+
+//Lembretes
+Route.get('/lembretes', 'LembretesController.index')
+Route.post('/lembretes', 'LembretesController.store')
+Route.put('/lembretes/:id_lembrete', 'LembretesController.update')
+Route.delete('/lembretes/:id_lembrete', 'LembretesController.destroy')
